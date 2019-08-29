2476161c6a98801d47df5da08841d77b680e1ffe
Julian Zucker <jzucker@pivotal.io>
2019-08-29 17:46:20 -0600

    Pretty print tokens

--- cmd/print_scanner/main.go
+++ cmd/print_scanner/main.go
@@ -10,26 +10,33 @@ import (
 )
 
 func main() {
-	fs := token.NewFileSet()
 	filename := os.Args[1]
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fs := token.NewFileSet()
 	f := fs.AddFile(filename, fs.Base(), len(b))
 	var s scanner.Scanner
 	s.Init(f, b, nil, scanner.ScanComments)
-
 	for {
 		_, tok, lit := s.Scan()
 		if tok == token.EOF {
 			break
 		}
-		if tok == token.SEMICOLON {
+		if tok == token.SEMICOLON  {
 			fmt.Println()
 			continue
 		}
-		fmt.Printf("[%s \"%s\"] ", tok, lit)
+		if lit == "" {
+			fmt.Printf("%s ", tok)
+		} else {
+			fmt.Printf("[%s %s] ", tok, lit)
+		}
+
+		if tok == token.LBRACE {
+			fmt.Println()
+		}
 	}
 }
